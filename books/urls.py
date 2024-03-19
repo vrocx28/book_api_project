d5c406dfe7ff139b1dd0f24aa08ce7c319fe8ba8
Vaibhav <vaibhavbansal28@gmail.com>
2024-03-19 18:06:26 +0530

    refactor

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BookListView
 
 urlpatterns = [
-    path('all_books', BookListView.as_view(), name='all_books'),
+    path('all_books/', BookListView.as_view(), name='all_books'),
     # Add more URL patterns as needed
 ]
